9a512112a958ff0416f347a3e1d8276a21e46cf8
Andrew J Ford <972461+skizzay@users.noreply.github.com>
2022-03-16 08:51:29 -0400

    Simplify simple transition

--- src/main/cpp/skizzay/fsm/simple_transition.h
+++ src/main/cpp/skizzay/fsm/simple_transition.h
@@ -1,29 +1,15 @@
 #pragma once
 
-#include <skizzay/fsm/concepts.h>
+#include "skizzay/fsm/event.h"
+#include "skizzay/fsm/state.h"
 
 namespace skizzay::fsm {
 
-namespace simple_transition_details_ {
 template <concepts::state CurrentState, concepts::state NextState,
           concepts::event Event>
-struct impl {
+struct simple_transition {
   using current_state_type = CurrentState;
   using next_state_type = NextState;
   using event_type = Event;
-
-  template <concepts::machine_for<
-      impl<current_state_type, next_state_type, event_type>>
-                Machine>
-  constexpr bool accepts(current_state_type const &, Machine const &,
-                         event_type const &) const noexcept {
-    return true;
-  }
 };
-} // namespace simple_transition_details_
-
-template <concepts::state CurrentState, concepts::state NextState,
-          concepts::event Event>
-using simple_transition =
-    simple_transition_details_::impl<CurrentState, NextState, Event>;
 } // namespace skizzay::fsm
